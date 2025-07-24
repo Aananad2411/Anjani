df877795017992aa8f8f92972315466f84ffcf8d
Aananad2411 <aanandsahu127@gmail.com>
2025-07-24 10:09:51 +0530

    Update __main__.py

--- anjani/__main__.py
+++ anjani/__main__.py
@@ -14,7 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from anjani import app  # assuming app is your Client instance
+from anjani.core.anjani_bot import AnjaniBot
 
 if __name__ == "__main__":
+    app = AnjaniBot()
     app.run()
