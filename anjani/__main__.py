1a3e782bbb2a12b01db67821c20126ce8b245f33
Aananad2411 <aanandsahu127@gmail.com>
2025-07-24 09:45:49 +0530

    Update __main__.py

--- anjani/__main__.py
+++ anjani/__main__.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from anjani import main
+from anjani import app  # assuming app is your Client instance
 
 if __name__ == "__main__":
-    main.start()
+    app.run()
